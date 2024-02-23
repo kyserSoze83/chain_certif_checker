b600b9ac98ac8e6d5a83bf98381147ee71637efe
Your Name <jean-baptiste.relave@isen.yncrea.fr>
2024-02-23 19:38:41 +0100

    sign valid

--- testJb.py
+++ testJb.py
@@ -8,18 +8,20 @@ import vclib
 import fjb
 
 def main():
-    certificat_obj = fjb.initCertif(0, "./certs/educ.isen-mediterranee.fr.crt") # Générer un objet certificat
+    certificat_obj = fjb.initCertif(0, "./certs/ACCVRAIZ1.crt") # Générer un objet certificat
 
     if certificat_obj == None:
         print("Certificat file is invalid...")
-    # else:
-    #     # Vérifier la signature du certifcat:
-    #     if not certificat_obj.checkSign():
-    #         print("Certificat signature is invalid...")
-    #     else:
-    #         # Vérifier les paramètres du certificat:
-    #         if not certificat_obj.checkParam():
-    #             print("Certificat parameters are invalid...")
+    else:
+        # Vérifier la signature du certifcat:
+        if not certificat_obj.checkSign():
+            print("Certificat signature is invalid...")
+        # else:
+        #     # Vérifier les paramètres du certificat:
+        #     if not certificat_obj.checkParam():
+        #         print("Certificat parameters are invalid...")
+
+        certificat_obj.print()
 
 if __name__ == '__main__':
     main()
